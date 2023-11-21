08ac662e02d7abb20a807443385b5f7ee77c3cdc
Driany2003 <creo07@hotmail.com>
2023-11-21 07:43:07 -0500

    primer cambio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PAGINA_WEB_LUZ.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    $(document).ready(function () {
+        $(".btnAgregar").on("click", function () {
+            var targetId = $(this).data("target");
+            var cantidad = $("#" + targetId).val();
+            alert("Agregado al carrito: " + cantidad + " unidades.");
+        });
+    });
