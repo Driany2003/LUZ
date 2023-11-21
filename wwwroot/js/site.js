efc00f39945e8671bb629191cdd8506995b302e3
Driany2003 <creo07@hotmail.com>
2023-11-21 08:34:12 -0500

    subiendo V1

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,16 @@
 ﻿<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    $(document).ready(function () {
-        $(".btnAgregar").on("click", function () {
-            var targetId = $(this).data("target");
-            var cantidad = $("#" + targetId).val();
-            alert("Agregado al carrito: " + cantidad + " unidades.");
-        });
-    });
+
+var cantidadInput = $(".cantidad[data-id=" + id + "]");
+
+if (cantidadInput.length > 0) {
+    var cantidad = cantidadInput.val();
+ 
+    if (cantidad && cantidad > 0) {
+    
+        console.log("Agregado al carrito - ID: " + id + ", Cantidad: " + cantidad);
+    } else {
+        alert("Por favor, ingresa una cantidad válida.");
+    }
+} else {
+    alert("No se encontró el elemento de cantidad para el ID: " + id);
+}
